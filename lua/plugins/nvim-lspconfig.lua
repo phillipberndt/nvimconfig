5879862d488b8462306c6fd4cab11c80bda3d5cb
Phillip Berndt <phillip.berndt@googlemail.com>
2024-10-15 15:26:26 +0200

    Add a few shortcuts for LSPs

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,12 +2,19 @@ return {
     "neovim/nvim-lspconfig",
     event = { "BufRead", "BufNewFile" },
     config = function()
+        local attach = function(client, bufnr)
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = bufnr, desc = "Go to definition"})
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, {buffer = bufnr, desc = "Show references"})
+            vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {buffer = bufnr, desc = "Auto-format code"})
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer = bufnr, desc = "Hover"})
+        end
+
         local lsp = require("lspconfig")
         if vim.fn.executable("pylsp") then
-            lsp["pylsp"].setup { settings = {} }
+            lsp["pylsp"].setup { settings = {}, on_attach = attach }
         end
         if vim.fn.executable("clangd") then
-            lsp["clangd"].setup { filetypes = {"c", "cpp", "cc", "h" } }
+            lsp["clangd"].setup { filetypes = {"c", "cpp", "cc", "h" }, on_attach = attach }
         end
     end,
 }
