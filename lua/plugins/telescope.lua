0b3fc5bdd92876e89ec51eb5ae5869a83976fce5
Phillip Berndt <phillip.berndt@googlemail.com>
2024-10-21 21:04:29 +0200

    Improve telescope mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,5 +14,7 @@ return {
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, { desc = 'LSP symbols' })
+        vim.keymap.set('n', '<leader>fS', builtin.treesitter, { desc = 'Treesitter symbols' })
     end
 }
