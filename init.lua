1b54536b467a6ee808e35cab25972af5a44e002d
Phillip Berndt <phillip.berndt@googlemail.com>
2025-09-26 10:52:56 +0200

    Add tiny-code-actions

--- init.lua
+++ init.lua
@@ -49,3 +49,7 @@ require("lazy").setup({
 
 
 require("auto-executable").setup()
+
+vim.keymap.set("n", "<leader>ca", function()
+	require("tiny-code-action").code_action()
+end, { noremap = true, silent = true })
